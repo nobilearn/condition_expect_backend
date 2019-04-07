2661c8dbbc526dd0e675962fae5473469d81f78f
nobi <nobilearn@gmail.com>
2019-04-07 15:28:26 +0900

    [service] server connection test: OK.

--- service.py
+++ service.py
@@ -5,20 +5,79 @@ ndarray dataset creater by csv.
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import sys
-
+import os
 import numpy as np
 
+'''
 from data_csv import DataGiver
+'''
+import requests
+import csv
+from io import StringIO
+#'''
+
 from model import ModelMLP
+
+
+def get( host_domain, options_str):
+
+	url	= os.path.join( host_domain, options_str)
+	print( url)
+
+	response	= requests.get(	url)
+	'''
+	print( response)
+	print( type( response))
+	print( response.url)
+	print( response.status_code)
+	print( response.encoding)
+#	print( response.content)
+	print( len( response.text.strip()))
+	print( response.text)
+	'''
+
+	return	response.text
+
+
+def post( host_domain, options_str, value):
+
+	url	= os.path.join( host_domain, options_str)
+	print( url)
+
+	response	= requests.post(
+		url,
+		{'result': value} )
+
+	return	0 if response.status_code == 200 else response.status_code
+
+
+def get_nda( csv_text):
+
+	cf	= csv.reader( StringIO( csv_text))
+
+	list_data	= [line for line in cf]
+	print( list_data)
+
+	return	np.array( list_data[1:])
 
 
 def	main( argv) :
 
-	csv_name 	= argv[1]
+#	csv_name 	= argv[1]
+	host_domain = argv[1]
+#	host_port	= int( argv[2])
+	options_str	= argv[2] if len( argv) > 2 else ''
 
+	'''
+	GET
+	'''
+	'''
 	dc	= DataGiver( csv_name)
-
 	nda	= dc.get_data()
+	'''
+	csv_text	= get( host_domain, options_str)
+	nda			= get_nda( csv_text)
+#	'''
 #	print( nda)
 	print( nda.shape)
 
@@ -33,25 +92,41 @@ def	main( argv) :
 	'''
 
 	input_shape	= np.array( [len( x[0])])
-	print( input_shape)
-	print( input_shape.shape)
-	print( type( input_shape))
+#	print( input_shape)
+#	print( input_shape.shape)
+#	print( type( input_shape))
 
 	input_shape	= np.expand_dims( input_shape, axis = 1)
 	print( input_shape)
 
-#	sys.exit( 0)
+	mm	= ModelMLP( input_shape = (len( x[0]),))
 
-	mm	= ModelMLP( input_shape = (6,))
-
-	mm.train( x[:-1], y[:-1])
+	mm.train( x[:-1], y[:-1])		# -1: latest data.
 
 	print( x[-1])
-#	pred	= mm.predict( x)
+#	pred	= mm.predict( x)		# all data.
 	pred	= mm.predict( np.expand_dims( x[-1], axis = 0))
 	print( pred)
+
+	'''
+	POST
+	'''
+	err	= post( host_domain, options_str, pred)
+	print( err)
+
+	return	err
 
 
 if __name__ == "__main__" :
 
-	main( sys.argv)
+	while True :
+		err	= main( sys.argv)
+		if True :
+#		if err != 0 :
+			break
+
+#		'''
+#		sleep( 60)
+		'''
+		datetime
+		'''
